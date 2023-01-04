c246175287c20e28ed91c20dcd0e7d7eb37d7c0d
danakkii <117612063+danakkii@users.noreply.github.com>
2023-01-04 20:01:15 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,15 @@
+A, B, C = map(int, [3, 3, 3])
+D, E, F = map(int, [4, 4, 4])
+G, H, I = map(int, [5, 5, 5])
+J, K, L = map(int, [6, 6, 6])
+M, N, O = map(int, [7, 7, 7])
+P, Q, R, S = map(int, [8, 8, 8, 8])
+T, U, V = map(int, [9, 9, 9])
+W, X, Y, Z = map(int, [10, 10, 10, 10])
+
+
+n = input()
+time = []
+for i in range(len(n)):
+    time.append(eval(n[i]))
+print(sum(time))
