b975fef2fd8419e0dc2bfe1492eca13813f10a39
danakkii <117612063+danakkii@users.noreply.github.com>
2023-01-16 13:58:54 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -0,0 +1,14 @@
+N = int(input())  
+num = N           
+count = 0         
+ 
+while True:
+    a = num//10 #2
+    b = num %10 #6
+    c = (a+b)%10 #8
+    num = (b*10) + c #68
+    count += 1 
+    if(num == N):
+        break
+ 
+print(count)
